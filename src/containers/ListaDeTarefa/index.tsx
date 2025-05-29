a3795e340de39762c40da561535ca86e68541834
Danilo <danilofelix827@gmail.com>
2025-05-29 13:48:43 -0300

    criando os cards de tarefas

--- src/containers/ListaDeTarefa/index.tsx
+++ src/containers/ListaDeTarefa/index.tsx
@@ -1,7 +1,38 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+import * as enums from '../../utils/enums/Tarefa'
+
+const tarefas = [
+  {
+    titulo: 'Estudar',
+    descricao: 'Assistir a aula',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'Pagar conta',
+    descricao: 'Baixar Fatura',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
+  }
+]
+
 const ListaDeTarefas = () => (
-  <main>
+  <Container>
     <p>2 Tarefas marcadas como:</p>
-  </main>
+    <ul>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
+    </ul>
+  </Container>
 )
 
 export default ListaDeTarefas
